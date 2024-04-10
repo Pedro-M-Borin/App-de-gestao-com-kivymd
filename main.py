9fcea10237a7338ef12058b469e12814776f4cc0
Pedro-M-Borin <pedro.borin168@outlook.com>
2024-04-10 19:06:36 -0300

    atualização 0.3

---
+++ main.py
@@ -0,0 +1,109 @@
+import mysql.connector
+from kivy.uix.screenmanager import ScreenManager
+from kivymd.app import MDApp
+from kivymd.uix.screen import Screen
+from kivymd.uix.textfield import MDTextField
+from kivymd.uix.button import MDRaisedButton, MDFlatButton, MDIconButton
+from kivymd.uix.label import MDLabel
+from kivymd.uix.dialog import MDDialog
+from registrar import RegisterTab  # Importar a classe RegisterTab do arquivo registrar.py
+
+class LoginApp(MDApp):
+    def build(self):        
+        self.screen_manager = ScreenManager()
+
+        self.login_screen = Screen(name='login')
+        self.register_screen = RegisterTab(name='register')  # Usar a classe RegisterTab
+
+        self.title_label = MDLabel(
+            text="Gestão Festa Junina 2024",
+            halign="center",
+            pos_hint={'center_x': 0.5, 'top': 0.8},
+            size_hint_y=None,
+            height=50,
+            font_style='H5'
+        )
+
+        self.username = MDTextField(
+            hint_text="Usuário",
+            pos_hint={'center_x': 0.5, 'center_y': 0.6},
+            size_hint=(0.7, None),
+            width=200,
+            required=True,
+            line_color_focus=[0,0,0,1], # Adicionado para manter a cor da linha quando o campo está em foco
+            line_anim=False, # Adicionado para desativar a animação da linha
+        )
+        self.password = MDTextField(
+            hint_text="Senha",
+            pos_hint={'center_x': 0.5, 'center_y': 0.5},
+            size_hint=(0.7, None),
+            width=200,
+            required=True,
+            password=True,
+            line_color_focus=[0,0,0,1], # Adicionado para manter a cor da linha quando o campo está em foco
+            line_anim=False, # Adicionado para desativar a animação da linha
+        )
+
+        # Botão para mostrar/esconder a senha
+        self.show_password_button = MDIconButton(
+            icon="eye-off",
+            pos_hint={'center_x': 0.85, 'center_y': 0.5},
+            on_release=self.toggle_password_visibility
+        )
+
+        self.login_button = MDRaisedButton(
+            text="Login",
+            pos_hint={'center_x': 0.5, 'center_y': 0.4},
+            size_hint=(0.7, None),
+            width=200,
+            on_press=self.login
+        )
+
+        self.register_button = MDFlatButton(
+            text="Registrar-se",
+            pos_hint={'center_x': 0.5, 'center_y': 0.3},
+            size_hint=(0.7, None),
+            width=200,
+            on_press=self.switch_to_register_screen
+        )
+
+        self.return_to_login_button = MDFlatButton(
+            text="Retornar ao Login",
+            pos_hint={'center_x': 0.5, 'center_y': 0.2},
+            size_hint=(0.7, None),
+            width=200,
+            on_press=self.switch_to_login_screen
+        )
+
+        self.login_screen.add_widget(self.title_label)
+        self.login_screen.add_widget(self.username)
+        self.login_screen.add_widget(self.password)
+        self.login_screen.add_widget(self.show_password_button)  # Adicionando botão para mostrar senha
+        self.login_screen.add_widget(self.login_button)
+        self.login_screen.add_widget(self.register_button)
+
+        self.screen_manager.add_widget(self.login_screen)
+        self.screen_manager.add_widget(self.register_screen)  # Adicionar a tela de registro ao ScreenManager
+
+        return self.screen_manager
+
+    def toggle_password_visibility(self, instance):
+        # Alterna a visibilidade da senha
+        if self.password.password:
+            self.password.password = False
+            self.show_password_button.icon = "eye"
+        else:
+            self.password.password = True
+            self.show_password_button.icon = "eye-off"
+
+    def login(self, instance):
+        print("Login com usuário:", self.username.text, "e senha:", self.password.text)
+
+    def switch_to_register_screen(self, instance):
+        self.screen_manager.current = 'register'
+
+    def switch_to_login_screen(self, instance):
+        self.screen_manager.current = 'login'
+
+if __name__ == '__main__':
+    LoginApp().run()
---
+++ registrar.py
@@ -0,0 +1,207 @@
+import mysql.connector
+from kivy.uix.screenmanager import Screen
+from kivymd.uix.textfield import MDTextField
+from kivymd.uix.button import MDRaisedButton, MDFlatButton, MDIconButton
+from kivymd.uix.label import MDLabel
+from kivymd.uix.dialog import MDDialog
+
+class RegisterTab(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        # Conectar ao banco de dados MySQL
+        self.mydb = mysql.connector.connect(
+            host="127.0.0.1",
+            user="root",
+            password="ellieborin@123",
+            database="gpjunina"
+        )
+        self.mycursor = self.mydb.cursor()
+
+        self.title_label = MDLabel(
+            text="Registrar-se",
+            halign="center",
+            pos_hint={'center_x': 0.5, 'top': 0.8},
+            size_hint_y=None,
+            height=50,
+            font_style='H5'
+        )
+
+        self.admin_code = MDTextField(
+            hint_text="Código de Administrador",
+            pos_hint={'center_x': 0.5, 'center_y': 0.8},
+            size_hint=(0.7, None),
+            width=200,
+            required=True,
+        )
+
+        self.name_field = MDTextField(
+            hint_text="Nome",
+            pos_hint={'center_x': 0.5, 'center_y': 0.7},
+            size_hint=(0.7, None),
+            width=200,
+            required=True,
+        )
+
+        self.email = MDTextField(
+            hint_text="E-mail",
+            pos_hint={'center_x': 0.5, 'center_y': 0.6},
+            size_hint=(0.7, None),
+            width=200,
+            required=True,
+        )
+
+        self.password_register = MDTextField(
+            hint_text="Senha",
+            pos_hint={'center_x': 0.5, 'center_y': 0.5},
+            size_hint=(0.7, None),
+            width=200,
+            required=True,
+            password=True,
+        )
+
+        # Botão para mostrar/esconder a senha
+        self.show_password_button = MDIconButton(
+            icon="eye-off",
+            pos_hint={'center_x': 0.85, 'center_y': 0.5},
+            on_release=self.toggle_password_visibility
+        )
+
+        # Ícone de olho para confirmar senha
+        self.show_confirm_password_button = MDIconButton(
+            icon="eye-off",
+            pos_hint={'center_x': 0.85, 'center_y': 0.4},
+            on_release=self.toggle_confirm_password_visibility
+        )
+
+        self.confirm_password = MDTextField(
+            hint_text="Confirmar Senha",
+            pos_hint={'center_x': 0.5, 'center_y': 0.4},
+            size_hint=(0.7, None),
+            width=200,
+            required=True,
+            password=True,
+        )
+
+        self.register_button = MDRaisedButton(
+            text="Cadastrar",
+            pos_hint={'center_x': 0.5, 'center_y': 0.3},
+            size_hint=(0.7, None),
+            width=200,
+            on_press=self.register
+        )
+
+        self.return_to_login_button = MDFlatButton(
+            text="Retornar ao Login",
+            pos_hint={'center_x': 0.5, 'center_y': 0.2},
+            size_hint=(0.7, None),
+            width=200,
+            on_press=self.switch_to_login_screen
+        )
+
+       
+        self.add_widget(self.admin_code)
+        self.add_widget(self.name_field)
+        self.add_widget(self.email)
+        self.add_widget(self.password_register)
+        self.add_widget(self.show_password_button)  # Adicionando botão para mostrar senha
+        self.add_widget(self.show_confirm_password_button)  # Adicionando botão para mostrar senha de confirmação
+        self.add_widget(self.confirm_password)
+        self.add_widget(self.register_button)
+        self.add_widget(self.return_to_login_button)
+
+    def toggle_password_visibility(self, instance):
+        # Alterna a visibilidade da senha
+        if self.password_register.password:
+            self.password_register.password = False
+            self.show_password_button.icon = "eye"
+        else:
+            self.password_register.password = True
+            self.show_password_button.icon = "eye-off"
+
+    def toggle_confirm_password_visibility(self, instance):
+        # Alterna a visibilidade da senha de confirmação
+        if self.confirm_password.password:
+            self.confirm_password.password = False
+            self.show_confirm_password_button.icon = "eye"
+        else:
+            self.confirm_password.password = True
+            self.show_confirm_password_button.icon = "eye-off"
+
+    def register(self, instance):
+        # Obter o código de administrador
+        codigo_adm = self.admin_code.text
+
+        # Verificar se todos os campos foram preenchidos
+        if not codigo_adm or not self.name_field.text or not self.email.text or not self.password_register.text or not self.confirm_password.text:
+            # Exibir mensagem de erro
+            dialog = MDDialog(
+                text="Por favor, preencha todos os campos!",
+                buttons=[
+                    MDFlatButton(
+                        text="OK",
+                        on_release=lambda x: dialog.dismiss()
+                    )
+                ]
+            )
+            dialog.open()
+            return
+
+        # Verificar se o código de administrador é válido
+        if codigo_adm != "$#admingpjunina#$":
+            # Exibir mensagem de erro
+            dialog = MDDialog(
+                text="Código de administrador inválido",
+                buttons=[
+                    MDFlatButton(
+                        text="OK",
+                        on_release=lambda x: dialog.dismiss()
+                    )
+                ]
+            )
+            dialog.open()
+            return
+
+        # Se o código de administrador for válido e todos os campos estiverem preenchidos, continuar com o registro
+        nome = self.name_field.text
+        email = self.email.text
+        senha = self.password_register.text
+        confirmar_senha = self.confirm_password.text
+
+        # Verificar se as senhas coincidem
+        if senha != confirmar_senha:
+            print("As senhas não coincidem")
+            return
+
+        try:
+            # Inserir os dados na tabela administradores
+            sql = "INSERT INTO administradores (codigo_adm, nome, email, senha) VALUES (%s, %s, %s, %s)"
+            val = (codigo_adm, nome, email, senha)
+            self.mycursor.execute(sql, val)
+            self.mydb.commit()
+            print("Usuário registrado com sucesso")
+
+            # Exibir mensagem de sucesso
+            dialog = MDDialog(
+                text="Usuário cadastrado com sucesso!",
+                buttons=[
+                    MDFlatButton(
+                        text="OK",
+                        on_release=lambda x: dialog.dismiss()
+                    )
+                ]
+            )
+            dialog.open()
+
+            # Limpar os campos após o registro
+            self.admin_code.text = ""
+            self.name_field.text = ""
+            self.email.text = ""
+            self.password_register.text = ""
+            self.confirm_password.text = ""
+
+        except mysql.connector.Error as e:
+            print("Erro ao registrar usuário:", e)
+
+    def switch_to_login_screen(self, instance):
+        self.manager.current = 'login'
